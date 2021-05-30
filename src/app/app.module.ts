d115ce67175e8f1cd53191be5886bd4be9f34e7e
Joanna Skrzypczak <j.skrzypczak1998@gmail.com>
2021-05-30 14:36:55 +0200

    Created routed and added imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RecipiesComponent } from './pages/recipies/recipies.component';
+import { ListComponent } from './pages/list/list.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { ButtonComponent } from './elements/button/button.component';
+import { MatButtonModule } from '@angular/material/button';
+
+
+
+const appRoues: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'recipies', component: RecipiesComponent},
+  {path: 'list', component: ListComponent},
+]
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, RecipiesComponent, ListComponent, ButtonComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatToolbarModule, MatTabsModule, MatButtonModule, RouterModule.forRoot(appRoues, {enableTracing: true})],
   providers: [],
   bootstrap: [AppComponent],
 })
