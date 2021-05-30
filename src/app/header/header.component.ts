89490d611d9bda410ad7ee794ae513a62834edcf
Joanna Skrzypczak <j.skrzypczak1998@gmail.com>
2021-05-30 14:35:45 +0200

    Created header with nav

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  links = [
+    {
+      name: "Home",
+      route: "/"
+    },
+    {
+      name: "Recipies",
+      route: "/recipies"
+    },
+    {
+      name: "List",
+      route: "/list"
+    }
+  ];
+  activeLink = this.links[0];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
