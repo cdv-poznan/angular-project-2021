611dcaf9d535444bd72f1d4bd7b7fb8e3a8a1fed
Linux Kyrios <malinowskim92@protonmail.com>
2021-06-08 19:54:39 +0200

    created static.datasource.ts file with initial content for preparing to future datasource

---
+++ src/app/model/static.datasource.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Product } from './product.model';
+import { from, Observable } from 'rxjs';
+
+@Injectable()
+export class StaticDatasource {
+  private products: Product[] = [
+    new Product(1, 'Product 1', 'Kategoria 1', 'Produkt 1 (Kategoria 1', 100),
+    new Product(2, 'Product 2', 'Kategoria 1', 'Produkt 2 (Kategoria 1', 100),
+    new Product(3, 'Product 3', 'Kategoria 1', 'Produkt 3 (Kategoria 1', 100),
+    new Product(4, 'Product 4', 'Kategoria 1', 'Produkt 4 (Kategoria 1', 100),
+    new Product(5, 'Product 5', 'Kategoria 2', 'Produkt 5 (Kategoria 2', 100),
+    new Product(6, 'Product 6', 'Kategoria 2', 'Produkt 6 (Kategoria 2', 100),
+    new Product(7, 'Product 7', 'Kategoria 2', 'Produkt 7 (Kategoria 2', 100),
+    new Product(8, 'Product 8', 'Kategoria 2', 'Produkt 8 (Kategoria 2', 100),
+    new Product(9, 'Product 9', 'Kategoria 3', 'Produkt 9 (Kategoria 3', 100),
+    new Product(10, 'Product 10', 'Kategoria 3', 'Produkt 10 (Kategoria 3', 100),
+    new Product(11, 'Product 11', 'Kategoria 3', 'Produkt 11 (Kategoria 3', 100),
+    new Product(12, 'Product 12', 'Kategoria 3', 'Produkt 12 (Kategoria 3', 100),
+    new Product(13, 'Product 13', 'Kategoria 4', 'Produkt 13 (Kategoria 4', 100),
+    new Product(14, 'Product 14', 'Kategoria 4', 'Produkt 14 (Kategoria 4', 100),
+    new Product(15, 'Product 15', 'Kategoria 5', 'Produkt 15 (Kategoria 4', 100),
+  ];
+
+  getProducts(): Observable<Product[]> {
+    return from([this.products]);
+  }
+}
