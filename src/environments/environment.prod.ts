4e8e1b63e3fe0ac83645362450cb993aec831dee
Joanna Skrzypczak <j.skrzypczak1998@gmail.com>
2021-06-18 00:54:06 +0200

    Added firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true,
+  firebase: {
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    apiKey: "AIzaSyBKDTzhVWfkzjvE4KCODT-FSp3o6YGweQY",
+    authDomain: "baguetti-a0786.firebaseapp.com",
+    projectId: "baguetti-a0786",
+    storageBucket: "baguetti-a0786.appspot.com",
+    messagingSenderId: "286728548667",
+    appId: "1:286728548667:web:a0b0b4b7d204d8c03ec71e",
+    measurementId: "G-M15EP3RL8L"
+  }
 };
